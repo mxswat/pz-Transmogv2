3fa4ac408bd90afeac279a79aa4c6b6c9da0b5dc
mxswat <massimocacciapaglia.13@gmail.com>
2022-09-28 17:59:31 +0100

    Hack and items spawns

--- Contents/mods/Transmogv2/media/lua/shared/~Transmogv2.shared.lua
+++ Contents/mods/Transmogv2/media/lua/shared/~Transmogv2.shared.lua
@@ -1,17 +1,3 @@
-local function generateItemHeadText(Name, DisplayName)
-    -- Cosmetic = TRUE Disables the tooltip for insulation and shit
-    return string.format([[
-        item %s
-        {
-            Type = Clothing,
-            Cosmetic = TRUE,
-            DisplayName = %s,
-            DisplayCategory = Clothing,]], 
-        Name,
-        DisplayName
-    )..'\n';
-end
-
 local function generateHideBodyLocation(bodyLocation)
     return string.format([[
         item Hide_%s
@@ -24,18 +10,31 @@ local function generateHideBodyLocation(bodyLocation)
             BodyLocation = Hide_%s,
             Icon = NoseRing_Gold,
             Weight = 0,
-        }]], 
+        }]],
         bodyLocation,
         bodyLocation,
         bodyLocation
-    )..'\n';
+    ) .. '\n\n';
 end
 
-local function DoParamAndStr(item, param)
-	if item then
-		item:DoParam(param)
-	end
-	return '\t\t\t' .. param .. ',\n'
+local function generateTmogItem(name, displayname, bodylocation, iconName)
+    return string.format([[
+        item %s
+        {
+            Type = Clothing,
+            Cosmetic = TRUE,
+            DisplayName = Cosmetic %s,
+            DisplayCategory = Clothing,
+            ClothingItem = Belt,
+            BodyLocation = Transmog_%s,
+            Icon = %s,
+            Weight = 0,
+        }]],
+        name,
+        displayname,
+        bodylocation,
+        iconName
+    ) .. '\n\n';
 end
 
 local function generateTransmog(sm)
@@ -43,6 +42,7 @@ local function generateTransmog(sm)
     local function write(content)
         fsWriter:write(content)
     end
+
     write('/* THIS FILE IS PROGRAMATICALLY GENERATED, MANUAL CHANGES WILL BE OVERWRITTEN */\n')
     write('/* Thank you to OliPro for allowing me to base this code of his work */\n')
     write('module TransmogV2 {\n\timports { Base }\n\n')
@@ -51,28 +51,17 @@ local function generateTransmog(sm)
     print('-------START Generate LogTransmogV2--------')
     for i = 0, size do
         local item = allItems:get(i);
-        -- I need to use tostring, getType returns a Java Enum  
+        -- I need to use tostring, getType returns a Java Enum
         local isClothing = tostring(item:getType()) == "Clothing"
         local isNotTransmogged = item:getModuleName() ~= "TransmogV2"
         local isWorldRender = item:isWorldRender()
-        if isClothing and isNotTransmogged and isWorldRender then
-            local itemName = item:getName()..'_'..item:getModuleName()
-            local displayName  = 'Cosmetic '..item:getDisplayName()
-            local itemType = 'Clothing'
-            local iconName = item:getIcon()
-            local clothingItem = item:getClothingItem()
+        local isCosmetic = item:isCosmetic()
+        if isClothing and isNotTransmogged and isWorldRender and not isCosmetic then
+            local itemName    = item:getModuleName() .. '_' .. item:getName()
+            local displayName = item:getDisplayName()
+            local iconName    = item:getIcon()
             -- I might not need a worldStaticItem
-            local newItem = not sm:FindItem('TransmogV2.' .. itemName) and createNewScriptItem('TransmogV2', itemName, displayName, itemType, iconName)
-            -- Write after find
-            local header = generateItemHeadText(itemName, displayName)
-            write(header)
-            write(DoParamAndStr(newItem, 'ClothingItem = ' .. clothingItem))
-            write(DoParamAndStr(newItem, 'BodyLocation = Transmog_'..item:getBodyLocation()))
-            write(DoParamAndStr(newItem, 'Icon = ' .. iconName))
-            write(DoParamAndStr(newItem, 'Weight = 0'))
-            -- local worldStaticItem = item:InstanceItem(nil):getWorldStaticItem()
-            -- write(DoParamAndStr(newItem, 'WorldStaticModel = ' .. tostring(worldStaticItem)))
-            write('\t\t}\n\n')
+            write(generateTmogItem(itemName, displayName, item:getBodyLocation(), iconName))
         end
     end
 
@@ -82,20 +71,42 @@ local function generateTransmog(sm)
 
     print('-------START Generate BodyLocations--------')
 
-    write('/* Hide BodyLocations */\n')
+    write('\n\t\t/* Hide BodyLocations */\n\n')
 
     for i = 0, allLocSize do
         local ID = allLoc:get(i):getId()
-        group:getOrCreateLocation("Hide_"..ID)
-        group:setHideModel("Hide_"..ID, ID)
-        group:getOrCreateLocation("Transmog_"..ID);
+        group:getOrCreateLocation("Hide_" .. ID)
+        group:setHideModel("Hide_" .. ID, ID)
+        group:getOrCreateLocation("Transmog_" .. ID);
 
-        write(generateHideBodyLocation(bodyLocation))
+        write(generateHideBodyLocation(ID))
     end
 
-
     write('}\n')
-	fsWriter:close()
+    fsWriter:close()
 end
 
 generateTransmog(getScriptManager())
+
+function ApplyItemsFix()
+    local sm = getScriptManager();
+    local allItems = sm:getAllItems()
+    local size = allItems:size() - 1;
+    print('-------START ApplyItemsFix--------')
+    for i = 0, size do
+        local item = allItems:get(i);
+        -- I need to use tostring, getType returns a Java Enum
+        local isTransmogged = item:getModuleName() == "TransmogV2"
+        local nameToFind = string.gsub(item:getName(), "_", ".", 1) .. ''
+        local originalItem = sm:FindItem(nameToFind)
+        if isTransmogged and originalItem then
+            print('nameToFind'..nameToFind)
+            item:setClothingItemAsset(originalItem:getClothingItemAsset())
+        end
+    end
+
+    local player = getPlayer();
+    player:resetModelNextFrame();
+end
+
+Events.OnLoad.Add(ApplyItemsFix);
